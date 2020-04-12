2ce9182a4866398f7e7f012913fe83388cd48f10
net1506@naver.com <net1506@naver.com>
2020-04-12 23:25:09 +0900

    #6.1

--- rooms/admin.py
+++ rooms/admin.py
@@ -13,6 +13,39 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        ("Basic Info", {
+            "fields": (
+                "name", "description", "country", "address", "price"
+            ),
+        }),
+        ("Times", {
+            "fields": (
+                "check_in", "check_out", "instant_book"
+            ),
+        }),
+        ("Spaces", {
+            "fields": (
+                "guests",
+                "beds",
+                "bedrooms",
+                "baths"
+            ),
+        }),
+        ("More About the Sapce", {
+            "classes": ("collapse",),
+            "fields": (
+                "amenities",
+                "facilities",
+                "house_rules",
+            ),
+        }),
+        ("Last Details", {
+            "fields": ("host",)
+        }),
+    )
+
+
     list_display = (
         "name",
         "country",
@@ -27,9 +60,12 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
     )
 
-    list_filter = ("instant_book", "city","country", )
+    list_filter = ("instant_book", "host__superhost", "city", "country", "room_type", "amenities", "facilities", "house_rules")
 
     search_fields = ["city", "^host__username"]
+
+    # many tomany 만 가능한거야 이거는...
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
