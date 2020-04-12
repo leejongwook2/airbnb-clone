d33a655744416fdb478deb05dfbe0a081eb91bd3
net1506@naver.com <net1506@naver.com>
2020-04-12 23:33:29 +0900

    #6.2

--- rooms/admin.py
+++ rooms/admin.py
@@ -58,7 +58,10 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
+
+    # ordering = ("name", "price", "bedrooms")
 
     list_filter = ("instant_book", "host__superhost", "city", "country", "room_type", "amenities", "facilities", "house_rules")
 
@@ -66,6 +69,12 @@ class RoomAdmin(admin.ModelAdmin):
 
     # many tomany 만 가능한거야 이거는...
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    # self 는 class 고 self 는 현재 row를 가르킴
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+    count_amenities.short_description = "amenities count"
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
--- rooms/models.py
+++ rooms/models.py
@@ -79,6 +79,5 @@ class Room(core_models.TimeStampedModel):
     facilities = models.ManyToManyField("Facility", blank=True)
     house_rules = models.ManyToManyField("HouseRule", blank=True)
 
-
     def __str__(self):
         return self.name
