8b36905169f9b71fc2fb9711ecd0751af56c9b5a
net1506@naver.com <net1506@naver.com>
2020-04-12 22:30:38 +0900

    #5.2

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+class List(core_models.TimeStampedModel):
+
+    """ List Model Definition """
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    # list 가 방을 여러개 가질수 있다.. 그러면 그냥 manyto many 로 걸어주나 본데??
+    rooms = models.ManyToManyField("rooms.Room", blank=True)
+
+    def __str__(self):
+        return self.name
