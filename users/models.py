cc77ac33c369765d2f21c5f4b60f22f0b927289b
net1506@naver.com <net1506@naver.com>
2020-04-12 18:02:32 +0900

    #3.3

--- users/models.py
+++ users/models.py
@@ -17,9 +17,30 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KROEAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KROEAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
     # blank =True 로 주어야지... 유저 만들 때 안적어줘도 된다...실제로 회원가입시에... 기입이 필요하지 않아....
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, default=GENDER_MALE, max_length=10, null=True, blank=True)
     bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
 
 
