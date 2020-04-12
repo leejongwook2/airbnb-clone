ec6e0bc1b3b162bded1b13fcd97ef86ee103490c
net1506@naver.com <net1506@naver.com>
2020-04-12 18:31:42 +0900

    #3.6

--- users/models.py
+++ users/models.py
@@ -35,12 +35,12 @@ class User(AbstractUser):
     )
 
     # blank =True 로 주어야지... 유저 만들 때 안적어줘도 된다...실제로 회원가입시에... 기입이 필요하지 않아....
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, default=GENDER_MALE, max_length=10, null=True, blank=True)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, default=GENDER_MALE, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
-    birthdate = models.DateField(null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
 
 
