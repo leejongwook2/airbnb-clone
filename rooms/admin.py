7e98d4ddf7d5baec1a2766247bc48dfea7d55ef1
net1506@naver.com <net1506@naver.com>
2020-04-12 20:49:25 +0900

    #4.3

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+
+    """ ItemAdmin Definition """
+
+    pass
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
--- users/models.py
+++ users/models.py
@@ -43,4 +43,5 @@ class User(AbstractUser):
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
 
-
+    def __str__(self):
+        return self.username
