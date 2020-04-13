ee67c1422a1dad25cded13ed1177a85a32a36fb2
net1506@naver.com <net1506@naver.com>
2020-04-13 22:31:55 +0900

    #11.1

--- rooms/views.py
+++ rooms/views.py
@@ -3,8 +3,12 @@ from datetime import datetime
 from . import models
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-
+    page = request.GET.get("page")
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
     return render(request, "rooms/home.html", context={
         "rooms": all_rooms
     })
