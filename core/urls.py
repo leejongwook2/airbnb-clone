36b40a9efb85b5f2683362fd5593b81f44a9dc87
net1506@naver.com <net1506@naver.com>
2020-04-13 23:31:04 +0900

    #11.7

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,9 @@ from rooms import views as room_views
 
 app_name="core"
 
-# 요청하는것 , 요청에 반응하는 것
+# 요청하는것 , 요청에 반응하는 것 (이것들은 함수에만 반응함... 그래서 as_view() 가 필요한거야)
 urlpatterns = [
-    path("", room_views.all_rooms, name="home")
+    path("", room_views.HomeView.as_view(), name="home")
 ]
 
 
