dd022c0ee684d29eff17ba48ad79e7c922f42e65
net1506@naver.com <net1506@naver.com>
2020-04-15 15:19:57 +0900

    #12.1

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
+from django.urls import reverse
 
 class AbstractItem(core_models.TimeStampedModel):
 
@@ -82,6 +83,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -45,3 +45,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
