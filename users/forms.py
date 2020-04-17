6189ac4fd54a9ec6eca25248741772fa7d58c81e
net1506@naver.com <net1506@naver.com>
2020-04-17 14:20:21 +0900

    #14.3

--- users/forms.py
+++ users/forms.py
@@ -9,13 +9,14 @@ class LoginForm(forms.Form):
 
     # validation 체크 하고싶으면 valid_필드이름 이어야 한데
     # 이 함수에서 값이 있나 체크를 하는거구나 .. ㅇㅇ
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("passwrd")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-
-    def clean_password(self):
-        return "fsjfojsjo"
+            self.add_error("email", forms.ValidationError("User does not exist"))
