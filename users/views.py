04646191e319e94ae332f7225a6f051c8965605d
net1506@naver.com <net1506@naver.com>
2020-04-17 17:10:59 +0900

    #15.0

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from . import forms
 class LoginView(FormView):
 
     template_name = "users/login.html"
-    form_class = forms.LoginForm()
+    form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
@@ -26,3 +26,14 @@ def log_out(request):
     """ def login_view(request):
 
     if request.method == "GET": """
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "lee",
+        "last_name": "fklsks",
+        "email": "net1506@naver.com"
+    }
