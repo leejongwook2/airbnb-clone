8ef3c16bd7cd14a7ae4895dd8cf634acd195c745
net1506@naver.com <net1506@naver.com>
2020-04-18 18:40:23 +0900

    #15.2

--- users/models.py
+++ users/models.py
@@ -39,8 +39,8 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, default=GENDER_MALE, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KROEAN)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW)
     superhost = models.BooleanField(default=False)
 
     def __str__(self):
